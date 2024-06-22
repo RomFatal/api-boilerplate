00fcd47c211d3b1613857bc6c6044bf708d48e15
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2024-06-22 14:46:45 +0300

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "plugins": [
-    "import"
+  extends: 'airbnb-base',
+  plugins: [
+    'import',
   ],
-  "parser": "@babel/eslint-parser",
-  "rules": {
-    "indent": ["error", 2]
+  parser: '@babel/eslint-parser',
+  rules: {
+    indent: ['error', 2],
   },
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaVersion": 6,
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true
-    }
-  }
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+    },
+  },
 };
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -15,5 +15,5 @@ export default {
       type: DataTypes.DATE,
       allowNull: false,
     },
-  }
+  },
 };
